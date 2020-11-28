8b94cc06386c7bb27ec2e31f189f68ae398eaca0
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-11-28 23:15:43 +0200

    Initial commit

---
+++ src/main/java/me.petterim1.bannerpattern/Plugin.java
@@ -0,0 +1,57 @@
+package me.petterim1.bannerpattern;
+
+import cn.nukkit.block.BlockID;
+import cn.nukkit.blockentity.BlockEntity;
+import cn.nukkit.blockentity.BlockEntityBanner;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.inventory.PlayerInventory;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.BannerPattern;
+
+public class Plugin extends PluginBase implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        if (e.getAction() == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK || e.getAction() == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem().getId() == 434) {
+                if (e.getBlock().getId() == BlockID.STANDING_BANNER || e.getBlock().getId() == BlockID.WALL_BANNER) {
+                    BlockEntity be = e.getPlayer().getLevel().getBlockEntity(e.getBlock());
+                    if (be instanceof BlockEntityBanner) {
+                        BlockEntityBanner banner = (BlockEntityBanner) be;
+                        switch (e.getItem().getDamage()) {
+                            case 0:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_CREEPER, banner.getDyeColor()));
+                                break;
+                            case 1:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_SKULL, banner.getDyeColor()));
+                                break;
+                            case 2:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_FLOWER, banner.getDyeColor()));
+                                break;
+                            case 3:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_MOJANG, banner.getDyeColor()));
+                                break;
+                            case 4:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_BRICK, banner.getDyeColor()));
+                                break;
+                            case 5:
+                                banner.addPattern(new BannerPattern(BannerPattern.Type.PATTERN_CURLY_BORDER, banner.getDyeColor()));
+                                break;
+                            default:
+                                return;
+                        }
+                        PlayerInventory inv = e.getPlayer().getInventory();
+                        inv.decreaseCount(inv.getHeldItemIndex());
+                        banner.spawnToAll();
+                    }
+                }
+            }
+        }
+    }
+}
